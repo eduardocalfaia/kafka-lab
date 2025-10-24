b6dfc337bd65725e3d1bb96a8392a1d23d46c3b0
eduardocalfaia <eduardocalfaia@gmail.com>
2025-10-24 12:06:59 +0100

    Fix zookeeper version on zookeeper-starter.sh

--- kafka_migration_zk_to_kraft/images/zookeeper/init-sh/zookeeper-starter.sh
+++ kafka_migration_zk_to_kraft/images/zookeeper/init-sh/zookeeper-starter.sh
@@ -5,20 +5,20 @@ if [ $(hostname) == "zookeeper1" ]; then
     mkdir -p /mnt/all_logs/zookeeper_data && echo "1" > /mnt/all_logs/zookeeper_data/myid
     sleep 2
     echo "Starting Zookeeper on zookeeper1"
-    /mnt/apache-zookeeper-3.7.2-bin/bin/zkServer.sh start
+    /mnt/apache-zookeeper-3.9.4-bin/bin/zkServer.sh start
     tail -f /dev/null
 elif [ $(hostname) == "zookeeper2" ]; then
     echo "Formating logs path for Zookeeper on zookeeper2"
     mkdir -p /mnt/all_logs/zookeeper_data && echo "2" > /mnt/all_logs/zookeeper_data/myid
     sleep 2
     echo "Starting Zookeeper on zookeeper2"
-    /mnt/apache-zookeeper-3.7.2-bin/bin/zkServer.sh start
+    /mnt/apache-zookeeper-3.9.4-bin/bin/zkServer.sh start
     tail -f /dev/null
 elif [ $(hostname) == "zookeeper3" ]; then
     echo "Formating logs path for Zookeeper on zookeeper3"
     mkdir -p /mnt/all_logs/zookeeper_data && echo "3" > /mnt/all_logs/zookeeper_data/myid
     sleep 2
     echo "Starting Zookeeper on zookeeper3"
-    /mnt/apache-zookeeper-3.7.2-bin/bin/zkServer.sh start
+    /mnt/apache-zookeeper-3.9.4-bin/bin/zkServer.sh start
     tail -f /dev/null
 fi
